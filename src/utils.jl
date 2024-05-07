fdc054e7800dc4b10f07ffb46d67e0e9cb189ce4
Logan Mondal Bhamidipaty <76822456+FlyingWorkshop@users.noreply.github.com>
2024-05-07 13:33:39 -0700

    epca changes

--- src/utils.jl
+++ src/utils.jl
@@ -4,6 +4,7 @@ function _single_fit_iter(L::Function, V, A, verbose::Bool, i::Integer, steps_pe
     return V, A
 end
 
+
 function _single_compress_iter(L::Function, V, A, verbose::Bool, i::Integer, steps_per_print::Integer, maxiter::Integer)
     result = optimize(A_hat->L(A_hat * V), A)
     A = Optim.minimizer(result)
@@ -14,10 +15,13 @@ function _single_compress_iter(L::Function, V, A, verbose::Bool, i::Integer, ste
     return A
 end
 
-function _fit!(epca::EPCA, X, maxoutdim::Integer, L::Function, maxiter::Integer, verbose::Bool, steps_per_print::Integer)
+
+function _fit!(epca::EPCA, X, maxoutdim::Integer, L::Function, maxiter::Integer, verbose::Bool, steps_per_print::Integer, A_init)
     n, d = size(X)
-    A = ones(n, maxoutdim)
     V = ismissing(epca.V) ? ones(maxoutdim, d) : epca.V
+    @assert size(V) == (maxoutdim, d)
+    A = isnothing(A_init) ? ones(n, maxoutdim) : A_init
+    @assert size(A) == (n, maxoutdim)
     for i in 1:maxiter
         V, A = _single_fit_iter(L, V, A, verbose, i, steps_per_print, maxiter)
     end
@@ -26,10 +30,11 @@ function _fit!(epca::EPCA, X, maxoutdim::Integer, L::Function, maxiter::Integer,
 end
 
 
-function _compress(epca::EPCA, X, L::Function, maxiter::Integer, verbose::Bool, steps_per_print::Integer)
+function _compress(epca::EPCA, X, L::Function, maxiter::Integer, verbose::Bool, steps_per_print::Integer, A_init)
     n, _ = size(X)
     outdim = size(epca.V)[1]
-    A = ones(n, outdim)
+    A = isnothing(A_init) ? ones(n, outdim) : A_init
+    @assert size(A) == (n, outdim)
     for i in 1:maxiter
         A = _single_compress_iter(L, epca.V, A, verbose, i, steps_per_print, maxiter)
     end
@@ -37,16 +42,16 @@ function _compress(epca::EPCA, X, L::Function, maxiter::Integer, verbose::Bool, 
 end
 
 
-function fit!(epca::EPCA, X; maxoutdim=1, maxiter=10, verbose=false, steps_per_print=10)
+function fit!(epca::EPCA, X; maxoutdim=1, maxiter=10, verbose=false, steps_per_print=10, A_init=nothing)
     L = _make_loss(epca, X)
-    A =  _fit!(epca, X, maxoutdim, L, maxiter, verbose, steps_per_print)
+    A =  _fit!(epca, X, maxoutdim, L, maxiter, verbose, steps_per_print, A_init)
     return A
 end
 
 
-function compress(epca::EPCA, X; maxiter=10, verbose=false, steps_per_print=10)
+function compress(epca::EPCA, X; maxiter=10, verbose=false, steps_per_print=10, A_init=nothing)
     L = _make_loss(epca, X)
-    A = _compress(epca, X, L, maxiter, verbose, steps_per_print)
+    A = _compress(epca, X, L, maxiter, verbose, steps_per_print, A_init)
     return A
 end
 
